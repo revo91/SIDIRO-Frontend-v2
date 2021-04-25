afa1eec2e91a33c601773f46e267ea34c585b68e
revo91 <p.kosowski91@gmail.com>
2021-04-25 13:15:59 +0200

    timeseries worker performance fix

--- src/workers/TimeseriesData.worker.js
+++ src/workers/TimeseriesData.worker.js
@@ -1,9 +1,12 @@
 import { fetchTimeseriesLastValue } from '../services/FetchTimeseriesAPI.service';
 /* eslint-disable */
 
-let interval;
+let interval1min;
+let interval15min;
 
 self.addEventListener("message", message => {
+  let promises1min = []
+  let promises15min = []
   let promises = []
   const { data } = message;
   //once instantly
@@ -15,15 +18,23 @@ self.addEventListener("message", message => {
     postMessage(res)
   })
   //intervally every x seconds
-  interval = setInterval(() => {
-    promises = []
+  interval1min = setInterval(() => {
+    promises1min = []
     data.deviceData.forEach((device) => {
-      promises.push(fetchTimeseriesLastValue(device.assetID, 1))
-      promises.push(fetchTimeseriesLastValue(device.assetID, 15))
+      promises1min.push(fetchTimeseriesLastValue(device.assetID, 1))
     })
-    Promise.all(promises).then(res => {
+    Promise.all(promises1min).then(res => {
       postMessage(res)
     })
   }, 60000) // 1 minute
+  interval15min = setInterval(() => {
+    promises15min = []
+    data.deviceData.forEach((device) => {
+      promises15min.push(fetchTimeseriesLastValue(device.assetID, 15))
+    })
+    Promise.all(promises15min).then(res => {
+      postMessage(res)
+    })
+  }, 3000000) // 15 minutes
 });
 
